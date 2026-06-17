5311952295056b34d1aeba5122cb6ec680645e13
agungperamerta <gedeagung068@gmail.com>
2026-06-17 17:04:19 +0700

    Add unit tests

--- src/test/java/com/AppTest.java
+++ src/test/java/com/AppTest.java
@@ -4,35 +4,62 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    public AppTest(String testName) {
+        super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testAddition() {
+        App app = new App();
+        assertEquals(3, app.calculate("add(1,2)", null));
+    }
+
+    public void testSubtraction() {
+        App app = new App();
+        assertEquals(7, app.calculate("sub(10,3)", null));
+    }
+
+    public void testMultiplication() {
+        App app = new App();
+        assertEquals(20, app.calculate("mult(5,4)", null));
+    }
+
+    public void testDivision() {
+        App app = new App();
+        assertEquals(4, app.calculate("div(20,5)", null));
+    }
+
+    public void testNestedExpression() {
+        App app = new App();
+        assertEquals(14,
+                app.calculate("add(2,mult(3,4))", null));
+    }
+
+    public void testLetExpression() {
+        App app = new App();
+        assertEquals(8,
+                app.calculate("let(a,5,add(a,3))", null));
+    }
+
+    public void testNegativeResult() {
+        App app = new App();
+        assertEquals(-5,
+                app.calculate("sub(5,10)", null));
+    }
+
+    public void testDivideByZero() {
+        App app = new App();
+
+        try {
+            app.calculate("div(10,0)", null);
+            fail("Expected ArithmeticException");
+        } catch (ArithmeticException e) {
+            assertTrue(true);
+        }
     }
 }
